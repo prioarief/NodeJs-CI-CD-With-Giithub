1fb3b4ebe150a2750edb60649097d289bffc4139
prioarief <prioariefgunawangunawan@gmail.com>
2020-11-22 13:55:14 +0700

    fix test with jest

---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+const app = require('../app') // Link to your server file
+const supertest = require('supertest')
+const request = supertest(app)
+
+it('Call the / endpoint', async done => {
+    const res = await request.get('/')
+    expect(res.status).toBe(200)
+    expect(res.text).toBe('Hello World With CI/CD')
+    done()
+})
--- test/test.js
+++
@@ -1,8 +0,0 @@
-const request = require("supertest");
-const app = require("../app");
-
-describe("GET /", () => {
-  it("respond with Hello World With CI/CD", (done) => {
-    request(app).get("/").expect("Hello World With CI/CD", done);
-  })
-});
