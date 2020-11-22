4f55aac2b17d00499fa13963f4ceb0c04400d556
prioarief <prioariefgunawangunawan@gmail.com>
2020-11-22 12:21:57 +0700

    test ci/cd

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
  // Respond with "hello world" for requests that hit our root "/"
  app.get("/", function (req, res) {
-  return res.send("Hello World With CI CD");
+  return res.send("Hello World With CI/CD");
  });
 
  // listen to port 7000 by default
--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../app");
 
 describe("GET /", () => {
-  it("respond with Hello World With CI CD", (done) => {
-    request(app).get("/").expect("Hello World With CI CD", done);
+  it("respond with Hello World With CI/CD", (done) => {
+    request(app).get("/").expect("Hello World With CI/CD", done);
   })
 });
